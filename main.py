374bc7b63e6cc3a38cca98f5d9bd069f577f52ce
LiNaBagda <bagdasarlina@gmail.com>
2022-03-23 23:37:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def convert():
     # set font style and size for text 
     pdf.set_font("arial", size = 10) 
     # open test.txt file with read mode
-    text_file = open("C:/Users/Lina/Desktop/project.txt", "r", encoding='UTF-8') 
+    text_file = open(my_file, "r", encoding='UTF-8') 
     # use for loop to extract the text
     # from text_file and insert it into pdf cell 
     for text in text_file: 
